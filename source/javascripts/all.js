76a812ed403f067dd8a2c55e191e984b95dda1bd
Dan Tao <daniel.tao@gmail.com>
2013-10-28 17:19:58 -0700

    added progress bars during race

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -9,9 +9,10 @@
 var lodash = _.noConflict();
 
 window.addEventListener('load', function() {
-  Benchmark.options.maxTime = 0.5;
+  Benchmark.options.maxTime = 1.0;
 
-  var results = document.querySelector('table#results tbody');
+  var results  = document.querySelector('table#results tbody'),
+      progress = document.getElementById('progress');
 
   function createEditor(containerId) {
     var container = document.getElementById(containerId),
@@ -43,6 +44,7 @@ window.addEventListener('load', function() {
         suite  = new Benchmark.Suite();
 
     results.innerHTML = '';
+    progress.innerHTML = '';
 
     eval(addBenchmarkToSuite('Underscore', underscoreEditor));
     eval(addBenchmarkToSuite('Lo-Dash', underscoreEditor, function(code) {
@@ -50,8 +52,44 @@ window.addEventListener('load', function() {
     }));
     eval(addBenchmarkToSuite('Lazy.js', lazyEditor));
 
+    var cycles    = {},
+        topCycles = 0;
+
+    suite.forEach(function(benchmark) {
+      benchmark.on('cycle', function() {
+        var progressBar = progress.querySelector('.progress-bar[data-benchmark="' + benchmark.name + '"]');
+
+        if (!progressBar) {
+          progressBar = document.createElement('DIV');
+          progressBar.className = 'progress progress-striped active';
+          progress.appendChild(progressBar);
+
+          progressBar = progressBar.appendChild(document.createElement('DIV'));
+          progressBar.className = 'progress-bar';
+          progressBar.setAttribute('data-benchmark', benchmark.name);
+
+          progressBar.appendChild(document.createElement('SPAN'));
+        }
+
+        cycles[benchmark.name] = cycles[benchmark.name] || 0;
+        cycles[benchmark.name] += benchmark.count;
+        topCycles = Math.max(topCycles, cycles[benchmark.name]);
+
+        progressBar.querySelector('SPAN').innerHTML =
+          benchmark.name + ' &times; ' + cycles[benchmark.name];
+
+        Lazy(progress.querySelectorAll('.progress-bar')).each(function(bar) {
+          var benchmarkCycles = cycles[bar.getAttribute('data-benchmark')];
+          bar.style.width = Math.floor(benchmarkCycles / topCycles * 100) + '%';
+        });
+      });
+    });
+
     suite.on('cycle', function(e) {
       var benchmark = e.target;
+
+      var currentProgressBar = progress.querySelector('.progress.active');
+      currentProgressBar.className = 'progress';
 
       var resultRow = document.createElement('TR'),
           labelCell = document.createElement('TD'),
@@ -64,40 +102,44 @@ window.addEventListener('load', function() {
       results.appendChild(resultRow);
 
       if (results.children.length === 3) {
-        new Highcharts.Chart({
-          title: {
-            text: document.querySelector('input[name="title"]').value
-          },
-          chart: {
-            renderTo: 'chart',
-            type: 'bar'
-          },
-          xAxis: {
-            categories: ['Ops/second']
-          },
-          yAxis: {
-            title: false
-          },
-          series: [
-            {
-              name: 'Underscore',
-              data: [Number(results.children[0].querySelector('td:last-child').textContent)]
-            },
-            {
-              name: 'Lo-Dash',
-              data: [Number(results.children[1].querySelector('td:last-child').textContent)]
-            },
-            {
-              name: 'Lazy.js',
-              data: [Number(results.children[2].querySelector('td:last-child').textContent)]
-            }
-          ],
-          credits: {
-            enabled: false
-          }
-        });
+        setTimeout(function() {
+          progress.innerHTML = '';
 
-        button.removeAttribute('disabled');
+          new Highcharts.Chart({
+            title: {
+              text: document.querySelector('input[name="title"]').value
+            },
+            chart: {
+              renderTo: 'chart',
+              type: 'bar'
+            },
+            xAxis: {
+              categories: ['Ops/second']
+            },
+            yAxis: {
+              title: false
+            },
+            series: [
+              {
+                name: 'Underscore',
+                data: [Number(results.children[0].querySelector('td:last-child').textContent)]
+              },
+              {
+                name: 'Lo-Dash',
+                data: [Number(results.children[1].querySelector('td:last-child').textContent)]
+              },
+              {
+                name: 'Lazy.js',
+                data: [Number(results.children[2].querySelector('td:last-child').textContent)]
+              }
+            ],
+            credits: {
+              enabled: false
+            }
+          });
+
+          button.removeAttribute('disabled');
+        }, 1000);
       }
     });
 
