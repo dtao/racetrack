cd0a9fab4fae2ed52b88dd27bc30a7e7c0c8feca
Dan Tao <daniel.tao@gmail.com>
2013-10-30 08:44:25 -0700

    don't minify javascript

--- config.rb
+++ config.rb
@@ -6,11 +6,6 @@ set :images_dir, 'images'
 
 activate :relative_assets
 
-# Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
-  activate :minify_javascript
 end
